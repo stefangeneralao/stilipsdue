70caa7260e1ab1fbf2826705ae4db74b7fcbaf02
stefangeneralao <stefangeneralao@gmail.com>
2022-10-24 16:56:55 +0200

    Add ESLint as Github Action

--- app/src/utils/index.ts
+++ app/src/utils/index.ts
@@ -1,7 +1,14 @@
 export * from './tasks';
 
-export const transposeMatrix = (matrix: any[][]) =>
-  matrix.reduce(
-    (prev, next) => next.map((item, i) => (prev[i] || []).concat(next[i])),
-    []
-  );
+export const transposeMatrix = <T>(matrix: T[][]): T[][] => {
+  const allRowsHaveSameLength = matrix
+    .map((row) => row.length)
+    .every((length, _, rowLengths) => length === rowLengths[0]);
+
+  if (!allRowsHaveSameLength) {
+    throw new Error('All rows must have the same length');
+  }
+
+  const result = matrix[0].map((_, i) => matrix.map((row) => row[i]));
+  return result;
+};
