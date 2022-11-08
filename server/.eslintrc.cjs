a5b2546b70039635dce825326b07b090eb965902
stefangeneralao <stefangeneralao@gmail.com>
2022-11-08 18:05:29 +0100

    Update eslintrc

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -13,6 +13,9 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { ignoreRestSiblings: true },
+    ],
   },
 };
